e46070931fdc80abca66aa7a8850fd4bc1afdca8
BlackLee_home <webitlee@163.com>
2017-02-18 22:02:58 +0800

    修改了hibernate的多对一关联关系的demo

--- src/com/lyn/hibernate/n21/Test.java
+++ src/com/lyn/hibernate/n21/Test.java
@@ -17,6 +17,25 @@ public class Test {
 		Session session = sf.openSession();
 		Transaction transaction = session.beginTransaction();
 		
+		Customer customer1 =new Customer();
+		customer1.setName("Black Lee");
+		Order order1 = new Order();
+		order1.setOrderName("乐事薯片");
+		Order order2 = new Order();
+		order2.setOrderName("德芙巧克力");
+
+		//设定关联关系
+		order1.setCustomerId(customer1);
+		order2.setCustomerId(customer1);
+		//先插入1的一端再插入多的一端,共执行3条insert语句
+//		session.save(customer1);
+//		session.save(order1);
+//		session.save(order2);
+		
+		//先插入多的一端再出入1的一端，共执行5条语句，3条insert语句，2条update语句
+		session.save(order1);
+		session.save(order2);
+		session.save(customer1);
 		
 		
 		transaction.commit();
