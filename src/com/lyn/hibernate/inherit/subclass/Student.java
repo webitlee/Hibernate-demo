31babb4791e8845d6cd9ee2d3d04bc76ba995647
BlackLee_home <webitlee@163.com>
2017-02-25 22:04:43 +0800

    添加了hibernate的映射继承关系中subclass映射的deom

---
+++ src/com/lyn/hibernate/inherit/subclass/Student.java
@@ -0,0 +1,14 @@
+package com.lyn.hibernate.inherit.subclass;
+
+public class Student extends Person{
+	private String school;
+
+	public String getSchool() {
+		return school;
+	}
+
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	
+}
---
+++ src/com/lyn/hibernate/inherit/subclass/Test.java
@@ -0,0 +1,28 @@
+package com.lyn.hibernate.inherit.subclass;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+public class Test {
+
+	public static void main(String[] args) {
+		SessionFactory sf = null;
+		Configuration conf = new Configuration().configure();
+		ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(conf.getProperties()).buildServiceRegistry();
+		sf = conf.buildSessionFactory(serviceRegistry);
+		Session session = sf.openSession();
+		Transaction transaction = session.beginTransaction();
+		
+		//²åÈë²Ù×÷
+		
+		transaction.commit();
+		session.close();
+		sf.close();
+		
+	}
+
+}
